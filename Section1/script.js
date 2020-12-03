c6a419839127fc5cd2e30eb242120b25cbd2777a
eogan-sullivan <eogan.sullivan@outlook.com>
2020-12-03 19:12:20 +0000

    Commit of Lecture 10

--- Section1/script.js
+++ Section1/script.js
@@ -1,3 +1,22 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is fun!');
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
