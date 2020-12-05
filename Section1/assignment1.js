29cb97b153ee2c25c2b42f1a4f41eafd86136f97
eogan-sullivan <eogan.sullivan@outlook.com>
2020-12-05 17:43:56 +0000

    Basic Operator Asignments

--- Section1/assignment1.js
+++ Section1/assignment1.js
@@ -4,14 +4,22 @@ const continent = "Europe";
 let population = 5;
 const isIsland = true;
 let language;
+const description = 'Portugal is in ' + continent + ', and its 11 million people speak portuguese';
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
 console.log(typeof country);
-country.log(typeof population);
-country.log(typeof isIsland);
-country.log(typeof language);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 33);
+
+
 
 language = 'English';
