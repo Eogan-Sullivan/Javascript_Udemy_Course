5a9a54e1d13b7d89da5f6cd1a4e753fff7c58257
Eogan Sullivan <eogan.sullivan@outlook.com>
2020-12-09 17:55:48 +0000

    Lecture 34 Function Declarations vs Expressions

--- Section2/script.js
+++ Section2/script.js
@@ -8,7 +8,7 @@ if (hasDriversLicense) console.log(`I can drive`);
 
 //const interface = 'Audio';
 // const private = 534;
-*/
+
 
 function logger(){
     console.log(`My name is Eógan`);
@@ -29,3 +29,19 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
+*/
+
+//function declaration
+function calcAge1(birthYear){
+    return 2037 - birthYear;
+}
+
+console.log(calcAge1(1993));
+
+//function expression
+const calcAge2 = function (birthYear){
+    return 2037  -birthYear;
+}
+
+const age2 = calcAge2(1993);
+console.log(age2);
