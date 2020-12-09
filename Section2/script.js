e9571bd121935e2ff9cc219610e067c8606e0629
Eogan Sullivan <eogan.sullivan@outlook.com>
2020-12-09 18:04:30 +0000

    Lecture 35 Arrow functions

--- Section2/script.js
+++ Section2/script.js
@@ -29,7 +29,7 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
-*/
+
 
 //function declaration
 function calcAge1(birthYear){
@@ -45,3 +45,20 @@ const calcAge2 = function (birthYear){
 
 const age2 = calcAge2(1993);
 console.log(age2);
+*/
+
+
+//Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1993);
+console.log(age3);
+
+const yearsUntilRetirement =  (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1993, 'Eógan'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
