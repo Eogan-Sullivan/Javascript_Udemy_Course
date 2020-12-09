2dacf3c6c792c2b3b9805605ac848127d86ae44c
Eogan Sullivan <eogan.sullivan@outlook.com>
2020-12-09 17:50:59 +0000

    Lectures 31 to 33, Strict Mode and Functions

---
+++ Section2/script.js
@@ -0,0 +1,31 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
+
+//const interface = 'Audio';
+// const private = 534;
+*/
+
+function logger(){
+    console.log(`My name is Eógan`);
+} 
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor (apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+let appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
