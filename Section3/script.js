3d57054215685378e8076e15429dad9549749b5c
eogan-sullivan <eogan.sullivan@outlook.com>
2021-01-06 20:18:18 +0000

    Debugger lecture

--- Section3/script.js
+++ Section3/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 ///////////////////////////////////////
 // Using Google, StackOverflow and MDN
 
@@ -69,3 +70,47 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
+    value: 10,
+    //value: Number(prompt('Degrees celsius')),
+  };
+
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement,value);
+  //   console.error(measurement.value)
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A Identify
+console.log(measureKelvin());
+
+//using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
