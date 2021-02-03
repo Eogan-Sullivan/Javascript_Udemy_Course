59619576d67bab7936e2beb05a5ad8770103c675
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-03 22:12:57 +0000

    Spread Operator

--- Section9/script.js
+++ Section9/script.js
@@ -36,8 +36,55 @@ const restaurant = {
       `Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient1?"),
+  // prompt("Let's make pasta! Ingredient2?"),
+  // prompt("Let's make pasta! Ingredient3?"),
+];
+console.log(ingredients);
+restaurant.orderPasta(...ingredients);
+
+//Since es2018 spread operator works on Objects even though they are not literals
+const newRestaurant = { foundedIn: '1991', ...restaurant, founder: 'Luigi' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name, restaurant.name);
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// spread operator array
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy Array - shallow copy
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2+ Arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//Iterables arrays,strings,maps,sets but not objects
+const str = 'eogan';
+const letters = [...str, ' ', 's'];
+console.log(letters);
+console.log(...str);
+
+/*
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via de Sole, 21',
