3c860bdae12517e99d091be03ead2d8786c056b6
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-07 22:02:46 +0000

    Coding Challenge 2 minus bonus challenge

--- Section9/codingChallenge.js
+++ Section9/codingChallenge.js
@@ -67,3 +67,24 @@ printGoals(...game.scored);
 //7.
 team1 < team2 && console.log('Team1 is more likely to win');
 team1 > team2 && console.log('Team2 is more likely to win');
+
+//Coding Challenge 2
+//1
+for (const [i, player] of game.scored.entries()) {
+  console.log(`${player} scored goal ${i + 1}`);
+}
+
+//2
+let avg = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds) {
+  avg += odd;
+}
+avg /= odds.length;
+console.log(avg);
+
+//3
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
