119f144b1a47e936d62d69cfcff32ae8dc277455
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-07 22:48:59 +0000

    Map fundamentals

--- Section9/script.js
+++ Section9/script.js
@@ -25,30 +25,6 @@ const ordersSet = new Set([
   'Pizza',
 ]);
 
-console.log(ordersSet);
-console.log(new Set('Eogan'));
-
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-console.log(ordersSet);
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-//ordersSet.clear;
-
-for (const order of ordersSet) console.log(order);
-
-//Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(staffUnique).size);
-
-console.log(new Set('eogansullivan').size);
-
-/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -79,6 +55,63 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italy');
+console.log(rest.set(2, 'Lisbon Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest.has(2));
+console.log(rest.size);
+// rest.clear();
+// console.log(rest.size);
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.get(arr));
+
+/*
+console.log(ordersSet);
+console.log(new Set('Eogan'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+console.log(ordersSet);
+ordersSet.delete('Risotto');
+console.log(ordersSet);
+//ordersSet.clear;
+
+for (const order of ordersSet) console.log(order);
+
+//Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(staffUnique).size);
+
+console.log(new Set('eogansullivan').size);
+
+/*
 const properties = Object.keys(hours);
 console.log(properties);
 
