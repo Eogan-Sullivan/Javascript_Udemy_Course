44404bb73e48b06f1b13c6802a3c05e4e907babe
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-08 00:11:49 +0000

    Map iterations

--- Section9/script.js
+++ Section9/script.js
@@ -55,6 +55,41 @@ const restaurant = {
   },
 };
 
+const question = new Map([
+  ['question', 'What is the best programming language in the World?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try Again'],
+]);
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(hours));
+const hoursMap = new Map(Object.entries(hours));
+console.log(hoursMap);
+
+//Iteration
+question.get('question');
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+//convert map to array
+console.log(...question);
+//console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+
+/*
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze Italy');
