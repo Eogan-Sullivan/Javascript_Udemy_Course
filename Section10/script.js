b5def698903adc0bbc4582b45805c67f63c4cf21
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-10 18:45:30 +0000

    Passing arguments into functions

--- Section10/script.js
+++ Section10/script.js
@@ -1,6 +1,6 @@
 'use strict';
+/*
 const bookings = [];
-
 const createBooking = function (
   flightNum,
   numPassengers = 1,
@@ -25,3 +25,31 @@ createBooking('LM123', 2);
 createBooking('LM123', 5);
 
 createBooking('LM123', undefined, 1000);
+*/
+const flight = 'LM234';
+const eogan = {
+  name: 'Eogan Sullivan',
+  passport: 234345134,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LM999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 234345134) {
+    alert('Check In');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+// checkIn(flight, eogan);
+// console.log(flight);
+// console.log(eogan);
+
+// const flightNum = flight;
+// const passenger = eogan;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(eogan);
+checkIn(flight, eogan);
