17aef79e04e2aab551f36e1e78b7d98f56396957
eogan-sullivan <eogan.sullivan@outlook.com>
2021-02-10 18:12:47 +0000

    String Metods Practice and end of Section

--- Section9/script.js
+++ Section9/script.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const hours = {
@@ -54,6 +58,24 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🛑' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(36);
+  console.log(output);
+}
+
+/*
 console.log('a+very+nice+string'.split('+'));
 console.log('Eogan Sullivan'.split(' '));
 
