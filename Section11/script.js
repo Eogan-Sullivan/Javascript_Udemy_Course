1475b5e65969ce55ca4e626e2eb73acbe8c8e481
eogan-sullivan <eogan.sullivan@outlook.com>
2021-11-06 17:31:11 +0000

    ForEach loop

--- Section11/script.js
+++ Section11/script.js
@@ -75,6 +75,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
+
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log(arr.slice(2));
@@ -105,3 +107,21 @@ console.log([...arr, ...arr2]);
 
 //JOIN
 console.log(letters.join('-'));
+*/
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You depositied ${movement}`);
+  } else console.log(`You withdrew ${Math.abs(movement)}`);
+}
+
+console.log('--------------FOR EACH-------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1} You depositied ${mov}`);
+  } else console.log(`You withdrew ${Math.abs(mov)}`);
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
