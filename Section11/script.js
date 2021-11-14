f56533f02ab5194c6694ad6498f5e2a588f51b39
eogan-sullivan <eogan.sullivan@outlook.com>
2021-11-14 16:23:10 +0000

    Coding Challenge 3

--- Section11/script.js
+++ Section11/script.js
@@ -296,7 +296,6 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1, avg2);
-*/
 
 const eurToUsd = 1.1;
 const totalDepositsUSD = movements
@@ -305,3 +304,17 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositsUSD);
+*/
+
+//Coding Challenge 3
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
