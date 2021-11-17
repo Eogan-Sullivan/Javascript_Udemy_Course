3585403190807be5bc8ee048c7971f107751e95e
eogan-sullivan <eogan.sullivan@outlook.com>
2021-11-17 18:22:08 +0000

    Sort method

--- Section11/script.js
+++ Section11/script.js
@@ -61,9 +61,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -200,6 +202,13 @@ btnClose.addEventListener('click', function (e) {
     //Hide UI
     containerApp.style.opacity = 0;
   }
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -427,7 +436,6 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
-*/
 
 //flat move nested arry to one array
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
@@ -448,3 +456,22 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+*/
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Mark'];
+console.log(owners.sort());
+console.log(owners);
+
+//Numbers
+console.log(movements);
+
+// return < 0 A, B
+// return > 0 B, A
+movements.sort((a, b) => a - b);
+
+console.log(movements);
+
+movements.sort((a, b) => b - a);
+
+console.log(movements);
