e0aeee25ee21498158a172bfacefa23344bb8683
eogan-sullivan <eogan.sullivan@outlook.com>
2021-11-17 17:33:53 +0000

    findIndex

--- Section11/script.js
+++ Section11/script.js
@@ -133,7 +133,6 @@ btnLogin.addEventListener('click', function (e) {
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
-  console.log(currentAccount);
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     // Display UI and message
@@ -168,6 +167,25 @@ btnTransfer.addEventListener('click', function (e) {
   }
 
   updateUI(currentAccount);
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    inputClosePin.value = inputCloseUsername.value = '';
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
