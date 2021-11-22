2494690307e28e5fcdd7ab158d479be6a8800569
Eogan Sullivan <eogan@L-PMEOGSUL01.fritz.box>
2021-11-22 21:08:12 +0000

    Passing Arguments to Event Handlers

--- Section13/script.js
+++ Section13/script.js
@@ -10,6 +10,14 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Navbar
+const nav = document.querySelector('.nav');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -55,12 +63,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e){
 })
 
 
-// Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
-
 tabsContainer.addEventListener('click', function (e){
   const clicked = e.target.closest('.operations__tab');
 
@@ -77,6 +79,25 @@ tabsContainer.addEventListener('click', function (e){
   //Active content area
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+//Menu face animation
+
+const handleHover = function (e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el != link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 
 
 
