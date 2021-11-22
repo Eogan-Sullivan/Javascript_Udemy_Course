c56e447fb4f7d0b02582ee6f1a9646289e55834a
Eogan Sullivan <eogan@L-PMEOGSUL01.fritz.box>
2021-11-22 15:51:25 +0000

    Event Types and Handlers

--- Section13/script.js
+++ Section13/script.js
@@ -35,17 +35,29 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function(e) {
   const s1coords = section1.getBoundingClientRect();
-
   // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
-  
   // window.scrollTo({
   //   left: s1coords.left + window.pageXOffset,
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: 'smooth'
   // })
-
   section1.scrollIntoView({behavior: 'smooth'});
 })
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e){
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter',alertH1);
+
+setTimeout(() => 
+h1.removeEventListener('mouseenter', alertH1),3000);
+
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Greate! You are reading the heading :D');
+// };
 
 
 
