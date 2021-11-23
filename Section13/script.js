57c5260837234dc9830526c4b80e2c4fe6acceb0
Eogan Sullivan <eogan@L-PMEOGSUL01.fritz.box>
2021-11-23 13:02:17 +0000

    Lifecycle DOM Evenets

--- Section13/script.js
+++ Section13/script.js
@@ -421,8 +421,18 @@ console.log(h1.parentElement.children);
 });
 */
 
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and Dom tree built', e);
+});
 
+window.addEventListener('load', function (e){
+  console.log('page loaded', e);
+})
 
-
+// window.addEventListener('beforeunload', function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// })
 
 
