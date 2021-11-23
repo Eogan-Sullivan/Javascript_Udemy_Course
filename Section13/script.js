2ea0355959c17427ba49d6d809e5f21875d744ff
Eogan Sullivan <eogan@L-PMEOGSUL01.fritz.box>
2021-11-23 12:56:43 +0000

    Slider Lectures

--- Section13/script.js
+++ Section13/script.js
@@ -188,6 +188,83 @@ const imgObserver = new IntersectionObserver(loading,{
 imgTargets.forEach(img => imgObserver.observe(img));
 
 
+//Slider 
+const slider = function(){
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const maxSlide = slides.length;
+const dotContainer = document.querySelector('.dots');
+let curSlide =0;
+
+//functions
+const goToSlide = function(slide){
+  slides.forEach((s, i)=> s.style.transform = `translateX(${100 * (i - slide)}%)`);
+}
+
+const nextSlide = function(){
+  if(curSlide === maxSlide-1){
+    curSlide = 0;
+  }
+  else{
+  curSlide++;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+
+const prevSlide = function(){
+  if(curSlide === 0){
+    curSlide = maxSlide -1;
+  }
+  else{
+  curSlide--;
+  }
+  goToSlide(curSlide)
+  activateDot(curSlide);
+};
+
+const createDots = function(){
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML('beforeend',`<button class="dots__dot" data-slide="${i}"></button>`);
+  });
+};
+
+const activateDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+
+const init = function(){
+createDots();
+activateDot(0);
+goToSlide(0);
+};
+
+init();
+
+//event handler
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e){
+  e.key === 'ArrowLeft' && prevSlide();
+  e.key === 'ArrowRight' && nextSlide();
+})
+
+dotContainer.addEventListener('click',function(e){
+  if(e.target.classList.contains('dots__dot')){
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+
+})
+};
+
+slider();
+
+
 
 // document.querySelectorAll('.nav__link').forEach(function (el){
 //   el.addEventListener('click',function(e){
