b51ab40897b0d3eef553b69a85b12e335aec5d04
eogan-sullivan <eogan.sullivan@outlook.com>
2021-11-24 22:39:00 +0000

    Project Architecture

--- Section15/script.js
+++ Section15/script.js
@@ -10,63 +10,88 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function(position){
- const {latitude} = position.coords;
- const {longitude} = position.coords;
+class App {
+  #map;
+  #mapEvent;
 
- const coords = [latitude,longitude];
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
- console.log(`https://www.google.ie/maps/@${latitude},${longitude}`);
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+  }
 
- map = L.map('map').setView(coords, 13);
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    const coords = [latitude, longitude];
 
-L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    console.log(`https://www.google.ie/maps/@${latitude},${longitude}`);
 
-// Handling clicks on map
-map.on('click', function(mapE){
-    mapEvent = mapE;
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    L.marker(coords)
+      .addTo(this.#map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
     form.classList.remove('hidden');
     inputDistance.focus();
+  }
 
-// console.log(mapEvent);
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-});
-
-},function(){
-    alert('Could not get your position');
-});
-
-form.addEventListener('submit', function(e){
-
+  _newWorkout(e) {
+    e.preventDefault();
     //clear inputfields
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
 
     //display marker
     e.preventDefault();
-    const {lat, lng} = mapEvent.latlng;
-    L.marker([lat,lng])
-    .addTo(map)
-    .bindPopup(L.popup({
-    maxWidth: 250,
-    minWidth: 100,
-    autoClose: false,
-    closeOnClick: false,
-    className: 'running-popup'
-    }))
-    .setPopupContent('Workout')
-    .openPopup();
-})
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
 
-inputType.addEventListener('change', function(){
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
+const app = new App();
